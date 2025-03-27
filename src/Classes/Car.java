378f924ea5499e4d41d8445ccd521ac42f03a321
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-03-27 16:17:02 -0300

    forgot to add some comments

--- src/Classes/Car.java
+++ src/Classes/Car.java
@@ -1,10 +1,12 @@
 package Classes;
 
 public class Car {
+    // fields
     public String brand = null;
     public String model = null;
     public String color = null;
 
+    // constructor
     public Car(String brand, String model, String color) {
         this.brand = brand;
         this.model = model;
--- src/Classes/Classes.java
+++ src/Classes/Classes.java
@@ -2,10 +2,12 @@ package Classes;
 
 public class Classes {
     public static void main(String[] args) {
+        // instances of the car object
         Car car1 = new Car("ferrari", "purosangue", "red");
         Car car2 = new Car("honda", "civic sport", "gray");
         Car car3 = new Car("toyota", "civic", "red");
 
+        // using methods
         car3.setModel("corolla");
         car1.setColor("yellow");
         car3.setColor("black piano");
