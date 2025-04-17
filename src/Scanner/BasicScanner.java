5a359a8e0eff3b2d9cf3506c3f5e6f1134b2b4cf
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-04-17 00:16:07 -0300

    scanner file to english + comments

--- src/Scanner/BasicScanner.java
+++ src/Scanner/BasicScanner.java
@@ -1,15 +1,27 @@
 package Scanner;
 
+// first import the scanner from java.util
 import java.util.Scanner;
 
 public class BasicScanner {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
+        // then create a new instance of scanner passing "system.in" to it, and store it on a variable of type Scanner.
+        Scanner reader = new Scanner(System.in);
 
-        System.out.println("Digite um valor abaixo");
+        System.out.println("type a value below");
 
-        int valorDigitado = leitor.nextInt();
+        // finally you can use it to grab the user input by calling the variable where scanner is stored and using.
+        // the method corresponding to the type you're expecting the user to enter.
+        // in this case I'm expecting a number of type int, so I use "nextInt".
+        int firstValueInput = reader.nextInt();
 
-        System.out.println("Valor digitado: " + valorDigitado);
+        System.out.println("type another value below");
+
+        // then of course you can use it multiple times.
+        int secondValueInput = reader.nextInt();
+
+        System.out.println("first value typed: " + firstValueInput);
+        System.out.println("second value typed: " + secondValueInput);
+
     }
 }
